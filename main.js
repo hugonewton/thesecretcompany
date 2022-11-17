67b6b03e0ce5deffaed7007753d1e2dc309b9e6a
hugonewton <hugovlnv@gmail.com>
2022-11-17 15:43:20 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 // bvcyusgvciuysg
+
+
+// PLAY VIDEO FROM TRACK RECORD SECTION
+
+  $('#play-btn-track-1').on('click', function(e) {
+    var $video = $('#video-track-record-1');
+    src = $video.attr('src');
+    $video.attr('src', src + '?autoplay=1');
+  });
