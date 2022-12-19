f7c6628e703139c5dba865efbdef00c4c7a51621
hugonewton <hugovlnv@gmail.com>
2022-12-19 18:05:33 +0100

    Update main.js

--- main.js
+++ main.js
@@ -9,24 +9,36 @@
   //   $video.attr('src', src + '?autoplay=1');
   // });
 
-  $('#play-btn-track-2').on('click', function(e) {
-    var $video = $('#video-track-record-2');
-    src = $video.attr('src');
-    $video.attr('src', src + '?autoplay=1');
-  });
-  $('#play-btn-track-3').on('click', function(e) {
-    var $video = $('#video-track-record-3');
-    src = $video.attr('src');
-    $video.attr('src', src + '?autoplay=1');
-  });
-  $('#play-btn-track-4').on('click', function(e) {
-    var $video = $('#video-track-record-4');
-    src = $video.attr('src');
-    $video.attr('src', src + '?autoplay=1');
-  });
+  // $('#play-btn-track-2').on('click', function(e) {
+  //   var $video = $('#video-track-record-2');
+  //   src = $video.attr('src');
+  //   $video.attr('src', src + '?autoplay=1');
+  // });
+  // $('#play-btn-track-3').on('click', function(e) {
+  //   var $video = $('#video-track-record-3');
+  //   src = $video.attr('src');
+  //   $video.attr('src', src + '?autoplay=1');
+  // });
+  // $('#play-btn-track-4').on('click', function(e) {
+  //   var $video = $('#video-track-record-4');
+  //   src = $video.attr('src');
+  //   $video.attr('src', src + '?autoplay=1');
+  // });
 
 
   var player = new Vimeo.Player(document.getElementById('video-track-record-1'));
   document.getElementById('play-btn-track-1').addEventListener('click', function() {
+    player.play();
+  });
+  var player = new Vimeo.Player(document.getElementById('video-track-record-2'));
+  document.getElementById('play-btn-track-2').addEventListener('click', function() {
+    player.play();
+  });
+  var player = new Vimeo.Player(document.getElementById('video-track-record-3'));
+  document.getElementById('play-btn-track-3').addEventListener('click', function() {
+    player.play();
+  });
+  var player = new Vimeo.Player(document.getElementById('video-track-record-4'));
+  document.getElementById('play-btn-track-4').addEventListener('click', function() {
     player.play();
   });
