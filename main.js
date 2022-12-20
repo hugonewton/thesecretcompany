9bcfdea0e7c675e9c9c4ebfc9ec2dce945c5da81
hugonewton <hugovlnv@gmail.com>
2022-12-20 17:18:35 +0100

    Update main.js

--- main.js
+++ main.js
@@ -30,14 +30,14 @@
   // document.getElementById('play-btn-track-1').addEventListener('click', function() {
   //   player.play();
   // });
-  var player2 = new Vimeo.Player(document.getElementById('video-track-record-2'));
-  document.getElementById('play-btn-track-2').addEventListener('click', function() {
-    player2.play();
-  });
-  var player3 = new Vimeo.Player(document.getElementById('video-track-record-3'));
-  document.getElementById('play-btn-track-3').addEventListener('click', function() {
-    player3.play();
-  });
+  // var player2 = new Vimeo.Player(document.getElementById('video-track-record-2'));
+  // document.getElementById('play-btn-track-2').addEventListener('click', function() {
+  //   player2.play();
+  // });
+  // var player3 = new Vimeo.Player(document.getElementById('video-track-record-3'));
+  // document.getElementById('play-btn-track-3').addEventListener('click', function() {
+  //   player3.play();
+  // });
 
   // var player4 = new Vimeo.Player(document.querySelector('#video-track-record-4 iframe'));
   // document.getElementById('play-btn-track-4').addEventListener('click', function() {
